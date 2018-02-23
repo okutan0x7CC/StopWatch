ca35731226ee687500f562ea35794df4dffa508c
MasakiOkuno <150441030@ccalumni.meijo-u.ac.jp>
2018-02-23 13:40:24 +0900

    [update] スリープ時にも動くようにしたが、startがリセット機能を持ってしまった

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -13,23 +13,30 @@ class ViewController: NSViewController {
     @IBOutlet weak var hourField: NSTextField!
     @IBOutlet weak var minField: NSTextField!
     @IBOutlet weak var secField: NSTextField!
-    @IBOutlet weak var buttonField: NSTextFieldCell!
+    @IBOutlet weak var buttonField: NSTextField!
     var stopWatch: StopWatchModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        stopWatch = StopWatchModel(hourField: hourField, minField: minField, secField: secField)
+        stopWatch = StopWatchModel(hourField: hourField,
+                                   minField: minField,
+                                   secField: secField)
+        
+        
     }
 
-    @IBAction func tapButton(_ sender: Any) {
-        if buttonField.stringValue != "stop" {
+    @IBAction func tapStartButton(_ sender: Any) {
+        if !stopWatch.isStarted {
             stopWatch.Start()
             buttonField.stringValue = "stop"
         } else {
             stopWatch.Stop()
-            stopWatch.Reset()
             buttonField.stringValue = "start"
         }
+    }
+    
+    @IBAction func tapResetButton(_ sender: Any) {
+        stopWatch.Reset()
     }
     
     override var representedObject: Any? {
--- StopWatch/model/StopWatchModel.swift
+++ StopWatch/model/StopWatchModel.swift
@@ -11,43 +11,56 @@ import Cocoa
 class StopWatchModel {
     
     var timer: Timer
-    var counter: Int
-    var hourField: NSTextField!
-    var minField: NSTextField!
-    var secField: NSTextField!
+    var startDate: Date!
+    var hourField: NSTextField
+    var minField: NSTextField
+    var secField: NSTextField
+    var isStarted: Bool
     
     init (hourField: NSTextField, minField: NSTextField, secField: NSTextField) {
         timer = Timer()
-        counter = 0
         self.hourField = hourField
         self.minField = minField
         self.secField = secField
+        isStarted = false
+        startDate = Date()
     }
 
     func Start() {
+        startDate = Date()
         timer = Timer.scheduledTimer(timeInterval: 1.0,
                                      target: self,
                                      selector: #selector(update),
                                      userInfo: nil,
                                      repeats: true)
+        isStarted = true
     }
 
     @objc private func update() {
-        counter += 1
-        let hour = Int(counter / 3600)
-        let min  = Int((counter - hour * 3600) / 60)
-        let sec  = counter - hour * 3600 - min * 60
+        let now = Date()
+        let (_, hour, min, sec) = calculateDifferenceTime(from: startDate, to: now)
         hourField.stringValue = NSString(format: "%02d", hour) as String
         minField.stringValue =  NSString(format: "%02d", min) as String
         secField.stringValue = NSString(format: "%02d", sec) as String
     }
+    
+    private func calculateDifferenceTime(from: Date, to: Date) -> (days: Int, hours: Int, minutes: Int, seconds: Int) {
+        let calendar    = Calendar.current
+        let s           = calendar.dateComponents([.second], from: from, to: to).second!
+        let days        = Int(s / 86400)
+        let hours       = Int((s - days * 86400) / 3600)
+        let minutes     = Int(((s - days * 86400 - hours * 3600)) / 60)
+        let seconds     = s - (minutes * 60) - (hours * 3600) - (days * 86400)
+        return (days, hours, minutes, seconds)
+    }
 
     func Stop() {
         timer.invalidate()
+        isStarted = false
     }
     
     func Reset() {
-        counter = 0
+        startDate = Date()
         hourField.stringValue = "00"
         minField.stringValue = "00"
         secField.stringValue = "00"
